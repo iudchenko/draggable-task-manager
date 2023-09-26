0c651837d927b75e4e20f93ecffef9ac547166af
iudchenko <andrii.iudchenko@gmail.com>
2023-09-26 16:18:55 +0200

    confirm button

--- src/components/ResetButton.tsx
+++ src/components/ResetButton.tsx
@@ -17,7 +17,7 @@ function ResetButton() {
             Cancel
           </button>
           <button
-            className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200"
+            className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-white focus:outline-none bg-red-500 rounded-lg border border-gray-200 hover:bg-red-300 focus:z-10 focus:ring-4 focus:ring-gray-200"
             onClick={() => {
               dispatch({
                 type: "reset",
