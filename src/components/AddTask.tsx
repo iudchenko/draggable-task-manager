fca1072d021d66f3cb272ebc3fe9eb5b458b460f
iudchenko <andrii.iudchenko@gmail.com>
2023-09-26 09:50:07 +0200

    button styling

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -33,7 +33,7 @@ export default function AddTask() {
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
-        <button className="flex gap-2 items-center text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
+        <button className="flex gap-2 items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-4 lg:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
           <HiOutlinePlus />
           <span>Add task</span>
         </button>
