6001f45fb3832a2c021d1b3d28c16636267bfeb3
iudchenko <andrii.iudchenko@gmail.com>
2023-09-26 16:13:30 +0200

    header fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import ResetButton from "./ResetButton";
 
 function Header() {
   return (
-    <header className="flex flex-col lg:flex-row justify-between p-5 max-w-4xl mx-auto gap-2">
+    <header className="flex flex-row justify-between p-5 max-w-4xl mx-auto gap-2">
       <h1 className="text-xl md:text-3xl font-bold justify-center">
         🎯Task Manager
       </h1>
